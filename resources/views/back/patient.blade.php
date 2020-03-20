322e2f5eaf25efef03f3e79b9531e6d32ba3299d
jiangyuan0222 <jiang.superstar222@outlook.com>
2020-03-20 01:20:59 +0100

    sdfs

--- resources/views/back/patient.blade.php
+++ resources/views/back/patient.blade.php
@@ -217,8 +217,14 @@
                         sel_ids.push(sel_id);
                     }
                     $("#status_ids").val(sel_ids);
+                      var title = '';
+                      if (status === 'published'){
+                        title = 'Publish';
+                      } else {
+                        title = 'UnPublish';
+                      }
                         Swal.fire({
-                          title: status,
+                          title: title,
                           text: msg,
                           type: "warning",
                           showCancelButton: true,
