92f2be3bd03f3b4e3eee7fe73b55091259addc5f
jiangyuan0222 <jiang.superstar222@outlook.com>
2020-03-28 18:59:02 +0100

    invoice part

--- database/migrations/2020_03_16_021300_create_invoices_table.php
+++ database/migrations/2020_03_16_021300_create_invoices_table.php
@@ -15,8 +15,23 @@ class CreateInvoicesTable extends Migration
     {
         Schema::create('invoices', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('invoice_id', 100);
+            $table->string('service_ids', 100);
+            $table->string('due_date', 100);
+            $table->string('appointment_date', 100);
+            $table->float('sub_total');
+            $table->float('total_discount');
+            $table->float('invoice_total');
+            $table->float('total_paid');
+            $table->float('total_due');
+            $table->string('payment_terms', 200)->nullable();
+            $table->string('client_notes', 200)->nullable();
+            $table->string('received_payment');
+            $table->string('payment_type', 200)->nullable();
+            $table->string('show_client_notes');
+            $table->string('show_payment_terms');
+            $table->string('is_sent');
             $table->string('civil_id', 190);
-            $table->integer('service_id');
             $table->string('status', 190)->default('published');
             $table->date('status_date')->nullable();
             $table->timestamps();
