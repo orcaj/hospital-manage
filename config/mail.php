aa69f8277715a7bbf587ac618c167dac503001d3
jiangyuan0222 <jiang.superstar222@outlook.com>
2020-04-06 05:48:33 +0200

    config

--- config/mail.php
+++ config/mail.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 465),
+    'port' => env('MAIL_PORT', 587),
 
     /*
     |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('sweden.super.star@gmail.com'),
+    'username' => env('MAIL_USERNAME'),
 
-    'password' => env('12345678aaa!@#'),
+    'password' => env('MAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
