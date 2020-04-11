310b0600e9bb6b6dc075f0227130f14986db4588
jiangyuan0222 <jiang.superstar222@outlook.com>
2020-04-11 22:09:28 +0200

    aw

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -457,7 +457,7 @@ class InvoiceController extends Controller
     }
 
     public function add_transaction_history(Request $request) {
-        $exist = Transaction::where('txnRefNo', $request->data['txnRefNo'])->get();
+        $exist = Transaction::where('txnRefNo', $request->data['txnRefNo'])->get()->count();
         print_r($exist);
         exit();
         if (!$exist) {
