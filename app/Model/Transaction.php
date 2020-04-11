8288e59bcc35c334975a97d36a494407303db7b9
jiangyuan0222 <jiang.superstar222@outlook.com>
2020-04-11 21:30:59 +0200

    model

--- app/Model/Transaction.php
+++ app/Model/Transaction.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     //
+    protected $fillable = [
+        'ecomTxnId','invoice_id' , 'createdDate' , 'amount', 'totalAmountDebittedFromCust','totalAmountCreditedToMerchant','clientRequestDate', 'clientConfirmationDate', 'processDate', 'tranStatus', 'walletTranStatus', 'merchantTxnRefNo', 'txnRefNo', 'merchantID', 'successUrl', 'failureUrl', 'crosscat', 'requestHashMac', 'custHashMac', 'paymentOptions', 'merchantName', 'paymentId', 'statusDescription'
+    ];
 }
